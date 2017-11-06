061389ab62ece337bec353cc311f3b583accad0d
Alejandro Garcia <33035163+wizagar@users.noreply.github.com>
2017-11-06 02:28:07 -0600

    Adding the project

---
+++ lift/src/Repository.java
@@ -0,0 +1,20 @@
+
+import com.google.gson.Gson;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class handles the Lift local repository opertions.
+ * 
+ * @author Alejandro Garcia
+ * @author Israel Segundo
+ */
+public class Repository {
+    
+    private List<File> files = new ArrayList<>();
+    
+    public static void createRepo() {
+        return;
+    }
+    
+}
